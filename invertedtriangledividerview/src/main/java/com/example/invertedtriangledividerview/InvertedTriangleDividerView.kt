b52b3b43ccbcc6a2ecfba4e46d7dbe96d56f8503
Anwesh43 <anweshthecool0@gmail.com>
2020-12-25 02:40:58 +0530

    adding constants and libraries

--- invertedtriangledividerview/src/main/java/com/example/invertedtriangledividerview/InvertedTriangleDividerView.kt
+++ invertedtriangledividerview/src/main/java/com/example/invertedtriangledividerview/InvertedTriangleDividerView.kt
@@ -1,2 +1,27 @@
 package com.example.invertedtriangledividerview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val parts : Int = 3
+val lines : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#009688",
+    "#3F51B5",
+    "#FF5722",
+    "#4CAF50"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 45f
