58354601b2b25a03b19c833df46f0b04d5151ab1
Anwesh43 <anweshthecool0@gmail.com>
2020-12-25 03:28:53 +0530

    adding ITDNode class

--- invertedtriangledividerview/src/main/java/com/example/invertedtriangledividerview/InvertedTriangleDividerView.kt
+++ invertedtriangledividerview/src/main/java/com/example/invertedtriangledividerview/InvertedTriangleDividerView.kt
@@ -127,4 +127,45 @@ class InvertedTriangleDividerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ITDNode(var i : Int, val state : State = State()) {
+
+        private var next : ITDNode? = null
+        private var prev : ITDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ITDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawITDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ITDNode {
+            var curr : ITDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
